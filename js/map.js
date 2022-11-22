8c2f64ef7ae3ccb6d65d25046c3609462efe2699
Alyona Rybkova <alyona.vorobyova.94@mail.ru>
2022-11-22 16:39:21 +0700

    Вносит корректировки, в соответствии с замечаниями проверяющего

--- js/map.js
+++ js/map.js
@@ -1,9 +1,10 @@
 import { enableUserForm } from './form.js';
 import { adForm } from './form.js';
 import { address } from './form-validate.js';
-import { createCardTemplate } from './create-card.js';
+import { createCardElement } from './create-card.js';
+import { init } from './main.js';
 
-const CENTER_COORDINATES = {
+export const CENTER_COORDINATES = {
   lat: 35.68950,
   lng: 139.69171,
 };
@@ -12,72 +13,58 @@ const DIGITS_IN_COORDINATE = 5;
 export const buttonSubmit = adForm.querySelector('.ad-form__submit');
 export const buttonReset = adForm.querySelector('.ad-form__reset');
 
-export const createMap = () => {
+// export const createMap = () => {
 
-  const mapScale = 12;
+const mapScale = 12;
 
-  //Создаем карту с координатами
-  const map = L.map('map-canvas')
-    .on('load', () => {
-      enableUserForm();
-    })
-    .setView(CENTER_COORDINATES, mapScale);
+//Создаем карту с координатами
+const map = L.map('map-canvas')
+  .on('load', () => {
+    enableUserForm();
+  })
+  .setView(CENTER_COORDINATES, mapScale);
 
-  //Добавляем слой с картой
-  L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    },
-  ).addTo(map);
+//Добавляем слой с картой
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
 
-  //Добавляем на карту метки
-  const mainPinIcon = L.icon({
-    iconUrl: './img/main-pin.svg',
-    iconSize: [52, 52],
-    iconAnchor: [26, 52],
-  });
+//Добавляем на карту метки
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
 
-  const pinIcon = L.icon({
-    iconUrl: './img/pin.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
+const pinIcon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
 
-  const mainPinMarker = L.marker(
-    CENTER_COORDINATES,
-    {
-      draggable: true,
-      icon: mainPinIcon,
-    },
-    address.value = `${CENTER_COORDINATES.lat} ${CENTER_COORDINATES.lng}`
-  );
+const mainPinMarker = L.marker(
+  CENTER_COORDINATES,
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  }
+);
 
-  mainPinMarker.addTo(map);
+mainPinMarker.addTo(map);
 
-  mainPinMarker.on('moveend', (evt) => {
-    const newCoordinates = evt.target.getLatLng();
-    address.value = `${newCoordinates.lat.toFixed(DIGITS_IN_COORDINATE)} ${newCoordinates.lng.toFixed(DIGITS_IN_COORDINATE)}`;
-  });
+mainPinMarker.on('moveend', (evt) => {
+  const newCoordinates = evt.target.getLatLng();
+  address.value = `${newCoordinates.lat.toFixed(DIGITS_IN_COORDINATE)} ${newCoordinates.lng.toFixed(DIGITS_IN_COORDINATE)}`;
+});
 
-  //Возвращает начальные координаты маркера при сбросе введенных данных и отправке формы
+//   // return {pinIcon, map};
+// };
+export const markerGroup = L.layerGroup().addTo(map);
 
-  const getInitialCoordinates = () => {
-    mainPinMarker.setLatLng(CENTER_COORDINATES);
-    map.setView(CENTER_COORDINATES, mapScale);
-    map.closePopup();
-    address.value = `${CENTER_COORDINATES.lat} ${CENTER_COORDINATES.lng}`;
-  };
-
-  buttonSubmit.addEventListener('click', getInitialCoordinates);
-
-  buttonReset.addEventListener('click', getInitialCoordinates);
-
-  return {pinIcon, map};
-};
-
-export const createMarkers = (similarAds, pinIcon, map) => {
-  const markerGroup = L.layerGroup().addTo(map);
+export const createMarkers = (similarAds) => {
 
   //Добавление меток объявлений на карту
   const createMarker = (similarAd) => {
@@ -93,12 +80,26 @@ export const createMarkers = (similarAds, pinIcon, map) => {
 
     marker
       .addTo(markerGroup)
-      .bindPopup(createCardTemplate(similarAd));
+      .bindPopup(createCardElement(similarAd));
   };
 
   similarAds.forEach((similarAd) => {
     createMarker(similarAd);
   });
 
-  return markerGroup;
+  // return markerGroup;
 };
+
+export const resetMarkersLayerGroup = () => markerGroup.clearLayers();
+
+//Сброс маркеров, карты и поля адреса
+
+export const resetCoordinates = () => {
+  mainPinMarker.setLatLng(CENTER_COORDINATES);
+  map.setView(CENTER_COORDINATES, mapScale);
+  map.closePopup();
+  address.value = `${CENTER_COORDINATES.lat} ${CENTER_COORDINATES.lng}`;
+  resetMarkersLayerGroup();
+  init();
+};
+
